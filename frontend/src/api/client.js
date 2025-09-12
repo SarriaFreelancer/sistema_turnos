9e2ee23038eacd42bb7140aa1113e892be2a9218
David Sarria <sarriafreelancer7@gmail.com>
2025-09-12 10:21:11 -0500

    Version 1.0.1: Desplegando 3.5

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,4 +1,3 @@
-// const API = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-const API = import.meta.env.VITE_API_URL || "https://mongodb-production-5671.up.railway.app/api";
-
+const API = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 export default API;
+
